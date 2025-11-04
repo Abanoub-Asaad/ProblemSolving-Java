144269fc07dae69d1b1daa2175f2b0a98f80a01f
abanoub-asaad <abanoub.asaad200010@gmail.com>
2025-11-04 11:54:18 +0200

    add 3 problems

---
+++ ProblemSolving/src/main/java/NeetCode/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,40 @@
+package NeetCode;
+
+//Time: O(N)
+//Space: O(1)
+public class RemoveDuplicatesFromSortedArray {
+
+    public int removeDuplicates(int[] nums) {
+        int k = 1;
+        int curInd = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i-1]) {
+                nums[curInd++] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
+
+/*
+
+//Time: O(N)
+//Space: O(N)
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        Set<Integer> uniqueSet = new LinkedHashSet<>();
+        for (int num : nums) {
+            uniqueSet.add(num);
+        }
+        int i = 0;
+        int k = uniqueSet.size();
+        for (Integer num : uniqueSet) {
+            nums[i++] = num;
+        }
+        return k;
+    }
+}
+
+*/
